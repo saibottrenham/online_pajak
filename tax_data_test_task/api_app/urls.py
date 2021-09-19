5008f8780b1bfc358029fb55b6948f929322c816
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-09-19 20:28:24 +1000

    Refined model to accept null entries, incorporated first endpoint

--- tax_data_test_task/api_app/urls.py
+++ tax_data_test_task/api_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TaxDataCommercialRelScoring, ThirdPartyVerification
 urlpatterns = [
     path('third-party/', ThirdPartyVerification.as_view()),
-    path('commercial-rel-scoring/<int:item_id>', TaxDataCommercialRelScoring.as_view()),
+    path('commercial-rel-scoring/', TaxDataCommercialRelScoring.as_view()),
 ]
--- tax_data_test_task/api_app/views.py
+++ tax_data_test_task/api_app/views.py
@@ -13,18 +13,11 @@ from .models import TaxDataModel
 class ThirdPartyVerification(View):
 
     def get(self, request):
-        items_count = TaxDataModel.objects.count() 
-        items = TaxDataModel.objects.all()  
-
-        items_data = []  
-        for item in items:
-            items_data.append(item)
-
-        data = {
-            'items': items_data,
-            'count': items_count,
-        }
-        return JsonResponse(data)
+        # Filter Db by company name. If it exist return
+        companies_query_set = TaxDataModel.objects.filter(company_name=request.GET.get('q'))
+        return JsonResponse(
+            {"message": "Exists"} if list(companies_query_set.values()) else {"message": "No Entries"}
+        )
            
 
 
